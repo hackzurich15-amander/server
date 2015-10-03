85ea94e38abc6e251beddfb9b675d3cc2e37c0fb
longstone <git@longstone.ch>
2015-10-03 12:26:23 +0200

    hack working

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var q = require('q');
 var app = express();
 var bodyParser = require('body-parser');
+var imgLoader = require('./loadImages');
 var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
@@ -11,7 +12,30 @@ var server = app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening at http://%s:%s', host, port);
 });
 
-var mysqlConf ={
+var buildWhere = function buildWhereF(fInclude) {
+    if (!fInclude) {
+        fInclude = {};
+    }
+    var qry = '';
+    if (fInclude.hasOwnProperty('priceMax')) {
+        qry += '`price` < ' + fInclude.priceMax;
+    }
+    if (fInclude.hasOwnProperty('psMin')) {
+        if (qry) {
+            qry += ' AND ';
+        }
+        qry += '`power_hp` > ' + fInclude.psMin;
+    }
+    if (fInclude.hasOwnProperty('brand')) {
+        if (qry) {
+            qry += ' AND ';
+        }
+        qry += '`brand` LIKE \'' + fInclude.brand + '\'';
+    }
+    return qry;
+};
+
+var mysqlConf = {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USR,
     password: process.env.MYSQL_PW,
@@ -19,8 +43,15 @@ var mysqlConf ={
     port: process.env.MYSQL_PORT
 };
 app.use(bodyParser.json());
-app.get('/',function(req,res){
-    res.send('heelloo');
+app.get('/', function (req, res) {
+    imgLoader.loadImg('TMBAJ7NP9G7006459').then(function (img) {
+        console.log('sucess', img);
+        res.send(img);
+    }, function (error) {
+        console.log('error', error);
+        res.send(error);
+    });
+
 });
 app.post('/magic', function (req, res) {
 
@@ -30,51 +61,63 @@ app.post('/magic', function (req, res) {
     var body = req.body || {};
     var trainSet = body.trainingSet || null;
     var net = new brain.NeuralNetwork();
+    var offset = body.offset || 0;
     if (trainSet) {
+        offset = trainSet.length;
         var learnSet = [];
-        trainSet.forEach(function(item){
-           learnSet.push( {input : [item.input.price] , output : { match : item.output.match}});
+        trainSet.forEach(function (item) {
+            learnSet.push({input: [item.input.price], output: {match: item.output.match}});
         });
 
         net.train(learnSet);
     }
-    var offset = body.offset || 0;
+
     var count = body.count || 100;
     var mysql = client.createConnection(mysqlConf);
-    mysql.table('vehicle').field(['vin', 'brand',
-        'model_de', 'engine_de',
-        'fuel_type',
-        'engine_capacity',
-        'power_hp',
-        'sale_type',
-        'emissions',
-        'additional_title',
-        'mileage',
-        'price',
-        'seats',
-        'sport_score',
-        'family_score',
-        'eco_score',
-        'price_score',
-        'offroad_score',
-        'design_score']).limit(offset, count).select().then(function (data) {
-        data.map(function (item) {
-            var _item = item;
-            delete _item.vin;
-            item.match = trainSet  ? net.run([_item.price]).match : null;
-        }).sort(function (a, b) {
-            if (a.match < b.match)
-                return -1;
-            if (a.match > b.match)
-                return 1;
-            return 0;
-        });
-        console.log('sortedArray');
-        res.json({data : data, offset : offset + count});
-    }).catch(function (e) {
-        console.log(e);
-        console.log(mysqlConf)
-    });
+    var whereQry = buildWhere(body.filterInclude);
+    console.log(whereQry);
+    mysql.table('vehicle').field(['vin', 'brand', 'model_de', 'engine_de', 'fuel_type', 'engine_capacity',
+        'power_hp', 'sale_type', 'emissions', 'additional_title', 'mileage', 'price', 'seats', 'sport_score',
+        'family_score', 'eco_score', 'price_score', 'offroad_score', 'design_score'])
+        .limit(offset, count).where(whereQry).select().then(function (data) {
+            data.map(function (item) {
+
+                var _item = item;
+                item.match = trainSet ? net.run([_item.price]).match : null;
+                return item;
+            }).sort(function (a, b) {
+                if (a.match < b.match)
+                    return -1;
+                if (a.match > b.match)
+                    return 1;
+                return 0;
+            });
+            var promisify = function (item) {
+                var deferred = q.defer();
+                imgLoader.loadImg(item.vin).then(function (imageUrl) {
+                    item.imageUrl = [imageUrl];
+                    console.log('image loaded for ' + item.vin + ' url: ' + imageUrl);
+                    deferred.resolve();
+                }, deferred.reject);
+                return deferred.promise;
+            };
+
+            var imgPromise = [];
+            for (i = 0; i < data.length; i++) {
+                imgPromise.push(promisify(data[i]));
+            }
+
+            q.allSettled(imgPromise)
+                .then(function (results) {
+                    res.json({data: data, offset: offset + count});
+                },function(err){
+                    res.json({error :err});
+                });
+
+}).catch(function (e) {
+    console.log(e);
+    console.log(mysqlConf)
+});
 
 });
 
