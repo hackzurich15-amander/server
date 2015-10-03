6c73273b4cc4a1df062847ab823c75fa81eb06ea
longstone <git@longstone.ch>
2015-10-03 13:17:07 +0200

    add thumbnail url instead of full

---
+++ loadImages.js
@@ -0,0 +1,26 @@
+/**
+ * Created by longstone on 03/10/15.
+ */
+var Q = require('q');
+var request = require('request');
+// .json
+
+
+
+// buz.js
+var LoadImages = function () {};
+
+LoadImages.prototype.loadImg = function (vid) {
+    var deferred = Q.defer();
+    request('http://api.hackzurich.amag.ch/hackzurich/vehicle/'+vid+'.json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+           var vehicleImages = JSON.parse(body).vehicleImages;
+            deferred.resolve('http://api.hackzurich.amag.ch/hackzurich/image/'+vehicleImages[0].id)+'/thumbnail';
+        }
+
+        deferred.reject(error)
+    });
+    return deferred.promise;
+};
+
+module.exports = new LoadImages();
