7719055e081455c26e43096937fd11e9f764385d
longstone <git@longstone.ch>
2015-10-06 22:16:31 +0200

    fix amag server is gone

--- loadImages.js
+++ loadImages.js
@@ -12,14 +12,20 @@ var LoadImages = function () {};
 
 LoadImages.prototype.loadImg = function (vid) {
     var deferred = Q.defer();
-    request('http://api.hackzurich.amag.ch/hackzurich/vehicle/'+vid+'.json', function (error, response, body) {
+    // workaround becaus the amag api is down
+    // amag api is down
+    deferred.resolve('http://www.gravatar.com/avatar/'+vid+'?s=400')
+
+/*    request('http://api.hackzurich.amag.ch/hackzurich/vehicle/'+vid+'.json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
            var vehicleImages = JSON.parse(body).vehicleImages;
+            console.log('loaded img: '+vehicleImages);
             deferred.resolve('http://api.hackzurich.amag.ch/hackzurich/image/'+vehicleImages[0].id+'/width/500');
+
         }
 
         deferred.reject(error)
-    });
+    });*/
     return deferred.promise;
 };
 
